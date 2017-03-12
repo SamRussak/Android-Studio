3e363c143a59f11a74bee91cf8eec44355f88484
Sam Russak <sjr59@pitt.edu>
2017-03-12 19:29:00 -0400

    add some button functionality

--- Calculator/app/src/main/java/com/example/sam/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/sam/calculator/MainActivity.java
@@ -13,21 +13,73 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Enter(View view) {
-    }
-
-    public void Add(View view) {
-    }
-    public void Sub(View view) {
-    }
-    public void Mult(View view) {
-    }
-    public void Divide(View view) {
-    }
 
     public void ClearScreen(View view) {
-        System.out.print("Here");
-        TextView text = (TextView) view.findViewById(R.id.textView);
+        TextView text = (TextView) findViewById(R.id.textView);
         text.setText("");
     }
+
+    public void Enter(View view) {
+
+    }
+
+    public void Plus(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append(" + ");
+    }
+    public void Minus(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append(" - ");
+    }
+    public void Mult(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append(" * ");
+    }
+    public void Divide(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append(" / ");
+    }
+
+    public void Zero(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("0");
+    }
+    public void One(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("1");
+    }
+    public void Two(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("2");
+    }
+    public void Three(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("3");
+    }
+    public void four(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("4");
+    }
+    public void Five(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("5");
+    }
+    public void Six(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("6");
+    }
+    public void Seven(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("7");
+    }
+    public void Eight(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("8");
+    }
+
+    public void Nine(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.append("9");
+    }
+
 }
